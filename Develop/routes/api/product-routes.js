46d411196fb74d08fd503939ccd17a8552519fca
shawnagohel <shawna.gohel@gmail.com>
2020-10-28 11:47:30 -0700

    routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -112,7 +112,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
 
   try {
